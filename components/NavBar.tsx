35a0edf01d6d2f5c2a2b19f4e22548c9d9a9892b
unknown <moraruedi11@outlook.com>
2026-01-15 12:17:48 +0100

    Initial Commit

---
+++ components/NavBar.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+} from "flowbite-react";
+import Link from "next/link";
+
+const NavBar = () => {
+  return (
+    <Navbar fluid rounded>
+      <NavbarBrand as={Link} href="https://flowbite-react.com">
+        <img
+          src="/favicon.svg"
+          className="mr-3 h-6 sm:h-9"
+          alt="Flowbite React Logo"
+        />
+        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+          Flowbite React
+        </span>
+      </NavbarBrand>
+      <NavbarToggle />
+      <NavbarCollapse>
+        <NavbarLink href="#" active>
+          Home
+        </NavbarLink>
+        <NavbarLink as={Link} href="#">
+          About
+        </NavbarLink>
+        <NavbarLink href="#">Services</NavbarLink>
+        <NavbarLink href="#">Pricing</NavbarLink>
+        <NavbarLink href="#">Contact</NavbarLink>
+      </NavbarCollapse>
+    </Navbar>
+  );
+};
+
+export default NavBar;
