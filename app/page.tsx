43c2353ecfad47aa1a0e85b3549e750b09cd7380
unknown <moraruedi11@outlook.com>
2026-02-11 16:30:17 +0100

    Added Card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,36 @@
 import React from "react";
 import TextBubble from "@/components/TextBubble";
 import AvatarBubble from "@/components/AvatarBubble";
+import { HomeCard } from "@/components/Cards";
+
 const page = () => {
   return (
-    <div className="mt-8 grid grid-cols-2 gap-4">
-      <div className="grid place-items-center">
-        <AvatarBubble imgSrc="/profile.jpg" alt="Profile" width={300} />
+    <div className="container mx-auto px-4 py-8">
+      <div className="mt-8 grid grid-cols-2 gap-4">
+        <div className="grid place-items-center">
+          <AvatarBubble imgSrc="/profile.jpg" alt="Profile" width={300} />
+        </div>
+        <div className="max-w-md">
+          <TextBubble>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </TextBubble>
+        </div>
       </div>
-      <div className="max-w-md">
-        <TextBubble>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </TextBubble>
+      <div className="mt-8 grid grid-cols-2 justify-items-center gap-4">
+        <div className="max-w-md">
+          <HomeCard
+            title="Card Title"
+            description="Go to da projectz page to see my projects cuh see more of this shiiiiiiiii cuhz"
+            buttonText="Learn More"
+          />
+        </div>
+        <HomeCard
+          title="Card Title"
+          description="Go to da about page to hit me up cuh what's up cuh yuh yuh"
+          buttonText="Learn More"
+        />
+        <div className="max-w-md"></div>
       </div>
     </div>
   );
---
+++ components/Cards.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Card as FlowbiteCard, Button } from "flowbite-react";
+import { HiArrowRight } from "react-icons/hi";
+
+interface CardProps {
+  title: string;
+  description: string;
+  buttonText: string;
+}
+
+export const HomeCard: React.FC<CardProps> = ({
+  title,
+  description,
+  buttonText,
+}) => {
+  return (
+    <FlowbiteCard>
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        {title}
+      </h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400">
+        {description}
+      </p>
+      <Button className="mt-4" onClick={() => alert("Button clicked!")}>
+        {buttonText} <HiArrowRight className="ml-2" />
+      </Button>
+    </FlowbiteCard>
+  );
+};
